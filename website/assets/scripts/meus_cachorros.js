88625246351c2ee2587a53da3af7b696c9abca34
Matheus Felipe <matheusfelipecxavier@gmail.com>
2024-05-23 14:01:31 -0300

    Update meus_cachorros.js

--- website/assets/scripts/meus_cachorros.js
+++ website/assets/scripts/meus_cachorros.js
@@ -1,27 +1,53 @@
-function ledados (){
-
+function ledados() {
     let strdados = localStorage.getItem('db');
-    let objdados = {};
+    let objdados = { animais: [] }; // Inicializa objdados com um array vazio
 
-    if (strdados){
-
-        objdados= JSON.parse (strdados);
+    if (strdados) {
+        objdados = JSON.parse(strdados);
     }
 
+    return objdados;
 }
 
-function imprimedados () {
+function salvardados(objdados) {
+    localStorage.setItem('db', JSON.stringify(objdados));
+}
 
+function excluirAnimal(index) {
+    let objdados = ledados();
+    objdados.animais.splice(index, 1); // Remove o animal do array
+    salvardados(objdados); // Salva o array atualizado no localStorage
+    imprimedados(); // Atualiza a interface
+}
+
+
+function imprimedados() {
     let tela = document.getElementById('tela');
     let strhtml = '';
-    let objdados = ledados ();
-}
+    let objdados = ledados();
 
-for (i=0; i < objdados.animal.length; i++){
-    strhtml = strhtml + `<p> ${objdados.animal[i].nome} - ${objdados.animal[i].raca} - ${objdados.animal[i].peso} - ${objdados.animal[i].personalidade}  </p>`
-    
+    if (objdados.animais && objdados.animais.length > 0) {
+        for (let i = 0; i < objdados.animais.length; i++) {
+            strhtml += `
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="assets/imgs/dog1.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${objdados.animais[i].nome}</h5>
+                        <p class="card-text">Raça: ${objdados.animais[i].raca} <br> Peso: ${objdados.animais[i].peso} <br> Personalidade: ${objdados.animais[i].personalidade}</p>
+                    </div>
+                    <div class="card-body">
+                        <a href="#" class="card-link text-warning" onclick="editarAnimal(${i})"><p><b>Editar Informações</b></p></a>
+                        <a href="#" class="card-link text-danger" onclick="excluirAnimal(${i})"><p><b>Excluir (caso foi adotado)</b></p></a>
+                    </div>
+                </div>
+            </div>`;
+        }
+    } else {
+        strhtml = '<p class="m-5"><b>Nenhum animal cadastrado.</b></p>';
+    }
+
     tela.innerHTML = strhtml;
-
 }
 
-document.getElementById ('btncarregadados').addEventListener ('click', imprimedados);
+document.getElementById('btncarregadados').addEventListener('click', imprimedados);
